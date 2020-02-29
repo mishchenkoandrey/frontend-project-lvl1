2c1ea0299ec44f9f8b79606c481d8fb46ab9cc7a
Andrey Mishchenko <buryonok@gmail.com>
2020-02-29 18:18:31 +0300

    test GihHub Actions

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { greeting } from '../src/index.js';
+import greeting from '../src/index.js';
 
 console.log('Welcome to the Brain Games!\n');
 
--- src/index.js
+++ src/index.js
@@ -5,4 +5,18 @@ const greeting = () => {
   console.log(`Hello, ${userName}!`);
 };
 
-export default greeting;
+const isEvenGame = () => {
+  const randomNum = Math.floor(Math.random() * 100);
+  const isEven = randomNum % 2 === 0;
+  const userAnswer = readlineSync.question(`Answer "yes" if the number is even, otherwise answer "no".\nQuestion: ${randomNum}`);
+  console.log(`Your answer: ${userAnswer}`);
+  if (userAnswer === 'yes') {
+    if (isEven) {
+      console.log('Correct!');
+    } else {
+      console.log(`"yes" is wrong answer ;(. Correct answer was "no".\nLet's try again, ${userName}!`);
+    }
+  }
+};
+
+export { greeting, isEvenGame };
