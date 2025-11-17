3b7306767fe8202f3c3311e5af2b111323695a5e
mishchenkoandrey <51906367+mishchenkoandrey@users.noreply.github.com>
2025-11-18 00:38:25 +0400

    Refactor ESLint config for ES2023 and cleanup
    Updated ESLint configuration to use ES2023 and removed unused imports.

--- eslint.config.js
+++ eslint.config.js
@@ -1,46 +1,16 @@
-import { globalIgnores } from 'eslint/config';
-import { FlatCompat } from '@eslint/eslintrc';
-import js from '@eslint/js';
-import globals from 'globals';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-
-const filename = fileURLToPath(import.meta.url);
-const dirname = path.dirname(filename);
-
-const compat = new FlatCompat({
-  baseDirectory: dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
+import globals from "globals";
 
 export default [
-  globalIgnores(['**/dist']),
-
-  ...compat.extends(
-    'airbnb-base',
-  ),
-
   {
     languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
+      ecmaVersion: 2023,
+      sourceType: "module",
       globals: {
         ...globals.node,
       },
     },
-
     rules: {
-      'no-console': 'off',
-      'import/extensions': 'off',
-    },
-  },
-
-  {
-    files: ['eslint.config.js'],
-    rules: {
-      'import/no-extraneous-dependencies': 'off',
-      'import/no-unresolved': 'off',
+      "no-console": "off",
     },
   },
 ];
