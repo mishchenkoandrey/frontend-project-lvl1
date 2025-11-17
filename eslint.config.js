b04d81174f9cda5845a93cd55ceb830e78a40896
mishchenkoandrey <51906367+mishchenkoandrey@users.noreply.github.com>
2025-11-18 00:20:41 +0400

    Refactor ESLint configuration with FlatCompat

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,49 @@
-import airbnbBase from "eslint-config-airbnb-base";
-import globals from "globals";
-import { FlatCompat } from "@eslint/eslintrc";
+import { globalIgnores } from 'eslint/config';
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import globals from 'globals';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
-const compat = new FlatCompat();
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
+const compat = new FlatCompat({
+  baseDirectory: dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
 
 export default [
+  globalIgnores(['**/dist']),
 
-  ...compat.extends("airbnb-base"),
+  ...compat.extends(
+    'airbnb-base',
+    'plugin:jest/recommended',
+  ),
 
   {
     languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
       globals: {
         ...globals.node,
+        ...globals.jest,
       },
     },
 
     rules: {
-      "no-console": "off",
-      "import/extensions": "off",
+      'no-console': 'off',
+      'import/extensions': 'off',
+      'jest/expect-expect': 'error',
+    },
+  },
+
+  {
+    files: ['eslint.config.js'],
+    rules: {
+      'import/no-extraneous-dependencies': 'off',
+      'import/no-unresolved': 'off',
     },
   },
 ];
